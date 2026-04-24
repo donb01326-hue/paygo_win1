913b8920e998cc2905b2830ec696b9cbc39cb40e
donb01326-hue <donb01326@gmail.com>
2026-04-24 06:48:41 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -30,7 +30,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("2086499772")
+    navigator.clipboard.writeText("OLU MESAGAN")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">2086499772</p>
+                <p className="font-bold">8154424186</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">KUDA</p>
+              <p className="font-bold">MONIEPOINT MFB</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">ENISAN MICHEAL</p>
+              <p className="font-bold">OLU MESAGAN</p>
             </div>
           </div>
 
